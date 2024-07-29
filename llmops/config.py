7006f3357c96ec4da227bd5c9f999fc4dcd106da
Maria Medina <mamedina@microsoft.com>
2024-07-29 13:44:12 +0200

    Polishing

--- llmops/config.py
+++ llmops/config.py
@@ -6,14 +6,21 @@ It determines the execution type of the package.
 It contains the following configuration:
 - EXECUTION_TYPE: The type of execution for the package.
   Valid values: LOCAL, AZURE
-  If environment variable LLMOPS_EXECUTION_TYPE is set, the value of the variable is used instead.
+  If environment variable LLMOPS_EXECUTION_TYPE is set,
+    its value is used instead.
 """
 
 import os
 
 try:
     EXECUTION_TYPE = os.environ["LLMOPS_EXECUTION_TYPE"]
-    print(f"Setting execution type from environment variable: {EXECUTION_TYPE}")
+    print(
+      "Setting execution type from environment variable:",
+      EXECUTION_TYPE
+    )
 except KeyError:
-    EXECUTION_TYPE = "AZURE"  # You can edit this value (LOCAL, AZURE)
-    print(f"Setting execution type from llmops module configuration: {EXECUTION_TYPE}")
+    EXECUTION_TYPE = "LOCAL"  # You can edit this value (LOCAL, AZURE)
+    print(
+      "Setting execution type from llmops module configuration:",
+      EXECUTION_TYPE
+    )
